da039ba3dfe8df63f4d87333ee1764f052a347b1
SeaDoggoWithInternet <100491220+SeaDoggoWithInternet@users.noreply.github.com>
2022-02-26 12:23:04 -0800

    Add files via upload

---
+++ Doodle fAll.py
@@ -0,0 +1,45 @@
+#import libraries
+import pygame
+
+#initialize pygame
+pygame.init()
+
+#game window dimensions
+S_WIDTH =400
+S_HIGHT=600
+
+#create the game window
+screen = pygame.display.set_mode((S_WIDTH, S_HIGHT)) #() is called a tuple, it is a data type.
+pygame.display.set_caption('FALL KNIGHT')
+
+#load images
+bg_image = pygame.image.load('bg.png').convert_alpha()
+char_image = pygame.image.load('very_green.png').convert_alpha()
+char_rekt = pygame.Rect(0,200,70,72)
+speed = 0
+#game loop
+run = True
+while run:
+    #draw the background
+    screen.blit(bg_image, (0, 0))
+    screen.blit(char_image, char_rekt)
+    #event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+ #       elif event.type==pygame.KEYDOWN:
+  #             speed=3
+   #         if event.key == pygame.K_a:
+    #            speed=-3
+     #   elif event.type==pygame.KEYUP:
+      #      if event.key==pygame.K_d:
+       #         speed=0
+        #    if event.key == pygame.K_a:
+         #       speed=0
+    char_rekt.x+=speed
+
+                
+    #update display window
+    pygame.display.update()
+
+pygame.quit()
